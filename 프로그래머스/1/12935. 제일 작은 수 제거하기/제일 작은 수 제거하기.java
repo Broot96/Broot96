d410198a002fbe3a0c234eed752c703896b2ac84
권석근 <121447330+Broot96@users.noreply.github.com>
2024-02-15 23:17:25 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.07 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,12 +1,24 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 class Solution {
-    public int[] solution(int[] arr) {
-        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
-        Arrays.sort(arr);
-        list.remove(list.indexOf(arr[0]));
-        if (list.size() <= 0) return new int[]{-1};
-        return list.stream().mapToInt(i->i).toArray();
-    }
+  public int[] solution(int[] arr) {
+      if(arr.length == 1){
+          int[] answer = {-1};
+          return answer;
+      }
+
+      int[] answer = new int[arr.length-1];
+      int minIndex=0;
+
+      for(int i=0;i<arr.length;i++){
+          if(arr[minIndex]>arr[i]){
+              minIndex = i;
+          }
+      }
+      for(int i=minIndex+1;i<arr.length;i++){
+          arr[i-1] = arr[i];
+      }
+      for(int i=0;i<answer.length;i++){
+          answer[i] = arr[i];
+      }
+      return answer;
+  }
 }
